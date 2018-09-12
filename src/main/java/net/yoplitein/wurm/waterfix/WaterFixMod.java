15efc5983c196fc4fa9e6cfbefd6b02f46fd7f61
Steven Dwy <yoplitein+git@gmail.com>
2018-09-11 22:46:27 -0700

    Initial commit

---
+++ src/main/java/net/yoplitein/wurm/waterfix/WaterFixMod.java
@@ -0,0 +1,19 @@
+package net.yoplitein.wurm.waterfix;
+
+import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
+import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
+
+public class WaterFixMod implements WurmServerMod, PreInitable
+{
+    @Override
+    public void preInit()
+    {
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("!!!!!!!!!!!!!!! Hello, World! !!!!!!!!!!!!!!!");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
