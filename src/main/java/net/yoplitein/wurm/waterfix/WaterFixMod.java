b906f84abebeb65289acf432d78f66cdbf1a891c
Steven Dwy <yoplitein+git@gmail.com>
2024-01-10 21:04:29 -0800

    Implement fix for hedges/flowerbeds at zero height and/or next to water

--- src/main/java/net/yoplitein/wurm/waterfix/WaterFixMod.java
+++ src/main/java/net/yoplitein/wurm/waterfix/WaterFixMod.java
@@ -9,6 +9,9 @@ import javassist.bytecode.CodeAttribute;
 import javassist.bytecode.CodeIterator;
 import javassist.bytecode.LocalVariableAttribute;
 import javassist.bytecode.MethodInfo;
+import javassist.expr.ExprEditor;
+import javassist.expr.MethodCall;
+import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
@@ -18,12 +21,17 @@ import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
 import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 
+import com.wurmonline.mesh.Tiles;
+import com.wurmonline.mesh.Tiles.TileBorderDirection;
+import com.wurmonline.server.Server;
+
 public class WaterFixMod implements WurmServerMod, PreInitable
 {
     @Override
     public void preInit()
     {
         fixUnderwaterCheck();
+        fixHedgeCheck();
     }
 
     private void fixUnderwaterCheck()
@@ -104,5 +112,48 @@ public class WaterFixMod implements WurmServerMod, PreInitable
             default:
                 throw new RuntimeException(String.format("Bytecode %02X is not a packed ILOAD", op));
         }
+    }
+
+    private void fixHedgeCheck()
+    {
+        try
+        {
+            ClassPool classPool = HookManager.getInstance().getClassPool();
+            CtClass terraforming = classPool.getCtClass("com.wurmonline.server.behaviours.Terraforming");
+            ExprEditor editor = new ExprEditor() {
+                @Override
+                public void edit(MethodCall call) throws CannotCompileException {
+                    if(call.getMethodName().equals("isCornerUnderWater")) {
+                        call.replace("{$_ = net.yoplitein.wurm.waterfix.WaterFixMod.isEdgeUnderWater(tilex, tiley, dir);}");
+                    }
+                }
+            };
+
+            CtMethod plantHedgeMethod = terraforming.getDeclaredMethod("plantHedge");
+            plantHedgeMethod.instrument(editor);
+
+            CtMethod plantFlowerbedMethod = terraforming.getDeclaredMethod("plantFlowerbed");
+            plantFlowerbedMethod.instrument(editor);
+        }
+        catch(NotFoundException | CannotCompileException err)
+        {
+            throw new HookException(err);
+        }
+    }
+
+    public static boolean isEdgeUnderWater(int tilex, int tiley, TileBorderDirection dir) {
+
+        short height = Tiles.decodeHeight(Server.surfaceMesh.getTile(tilex, tiley));
+        if(height < 0)
+            return true;
+
+        int dx = 0;
+        int dy = 0;
+        if(dir == TileBorderDirection.DIR_HORIZ)
+            dx = 1;
+        else
+            dy = 1;
+        height = Tiles.decodeHeight(Server.surfaceMesh.getTile(tilex + dx, tiley + dy));
+        return height < 0;
     }
 }
